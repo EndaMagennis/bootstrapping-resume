8ec792946a8e01e34b3b63e415fecb093777dd85
Enda <endamagennis@gmail.com>
2023-11-16 14:30:26 +0000

    add comments

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,4 +1,5 @@
 function initMap() {
+    // The location of Uluru
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
@@ -7,14 +8,17 @@ function initMap() {
         }
     });
 
+    // The marker, positioned at Uluru
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    // Add some markers to the map.
     var locations = [
         { lat: 40.785091, lng: -73.968285 },
         { lat: 41.084045, lng: -73.874245 },
         { lat: 40.754932, lng: -73.984016 },
     ];
 
+    // Add some markers to the map.
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
@@ -22,6 +26,7 @@ function initMap() {
         });
     });
 
+    // Add a marker clusterer to manage the markers.
     var markerCluster = new MarkerClusterer(map, markers, 
     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'})
 }
